2bfafa9687120fb3dae106f7501817d2e3ccebf5
MAXIMZOLOTYKH11 <max.zl@mail.ru>
2019-02-24 18:59:01 +0700

    committt

--- case_fuel.py
+++ case_fuel.py
@@ -1,8 +1,8 @@
 import random
 
 
-def time(car):
-    l = int(car['li'])
+def t(i):
+    l = int(i['li'])
     if (l % 10) == 0:
         t_1 = int(l // 10)
     else:
@@ -12,25 +12,30 @@ def time(car):
     time = t_1 + a
     if time <= 0:
         time = 1
-    return(time)
+    return time
 
 
-def azs(car, kolonki, counter):  # {'time1': '00:01', 'li': 10, 'fuel': 'АИ-80'}
+def azs(car, kolonki, counter, auto1, auto2, auto3):  # {'time1': '00:01', 'li': 10, 'fuel': 'АИ-80'}
     automat = car['fuel']  # АИ-80, azs.txt, 1
-    auto1 = 0
-    auto2 = 0
-    auto3 = 0
+    if counter == 1:
+        auto1 += 1
+    elif counter == 2:
+        auto2 += 1
+    if counter == 3:
+        auto3 += 1
+    c = 0
     for i in kolonki:
-        if counter == i[0]:
-            auto1 += 1
+        c += 1
+        if c == 1:
             print('Автомат №' + i[0] + ';', 'максимальная очередь:', i[2] + ';', 'Марки бензина:', i[4:], '->' + auto1 * '*')
-        elif counter == i[0]:
-            auto2 += 1
+        if c == 2:
             print('Автомат №' + i[0] + ';', 'максимальная очередь:', i[2] + ';', 'Марки бензина:', i[4:], '->' + auto2 * '*')
-        else:
-            auto3 += 1
+        if c == 3:
             print('Автомат №' + i[0] + ';', 'максимальная очередь:', i[2] + ';', 'Марки бензина:', i[4:], '->' + auto3 * '*')
-    return
+    a1 = auto1
+    a2 = auto2
+    a3 = auto3
+    return a1,a2,a3
 
 
 def main():
@@ -50,20 +55,45 @@ def main():
         kolonki = f_in2.readlines()
 
     # car = {'time1': '00:01', 'li': 10, 'fuel': 'АИ-80'}
-    for i in car_list:
-        time(i)
-
-    # l = car['li']
     count = 0
-    # a1 = {}
-    # a2 = {}
-    # a3 = {}
+    auto1 = 0
+    auto2 = 0
+    auto3 = 0
+    g = None
     for i in car_list:
         print(i)
         counter = 1
+        go = 100
+        cc = 0
+        go = int(i['time1'][3:]) + t(i)
         for r in kolonki:
             if i['fuel'] in r:
-                azs(i, kolonki, counter)
+                # azs(i, kolonki, counter, auto1, auto2, auto3)
+                if go < int(i['time1'][3:]):
+                    print('В', '00:'+str(go), 'клиент', car_list[cc]['time1'], car_list[cc]['fuel'], car_list[cc]['li'],'заправил свой автомобиль и покинул АЗС.')
+                    cc += 1
+                else:
+                    print('В', i['time1'], 'новый клиент:', i['time1'], i['fuel'], i['li'], t(i), 'встал в очередь к автомату №' + str(counter))
+                    if counter == 1:
+                        auto1 += 1
+                    elif counter == 2:
+                        auto2 += 1
+                    if counter == 3:
+                        auto3 += 1
+                    c = 0
+                    for l in kolonki:
+                        c += 1
+                        if c == 1:
+                            print('Автомат №' + l[0] + ';', 'максимальная очередь:', l[2] + ';', 'Марки бензина:', l[4:],
+                                  '->' + auto1 * '*')
+                        if c == 2:
+                            print('Автомат №' + l[0] + ';', 'максимальная очередь:', l[2] + ';', 'Марки бензина:', l[4:],
+                                  '->' + auto2 * '*')
+                        if c == 3:
+                            print('Автомат №' + l[0] + ';', 'максимальная очередь:', l[2] + ';', 'Марки бензина:', l[4:],
+                                  '->' + auto3 * '*')
+                    go = int(i['time1'][3:]) + t(i)
+                    g = i
             else:
                 counter += 1
         count += 1
